847e671ca2b0f6fdda151c0ff122f0df7f3e791b
Jack Domleo <jackdomleo7@gmail.com>
2020-12-18 21:40:13 +0000

    Installed initial Vue component library and Nuxt website

---
+++ domleo-vue/domleo-vue.d.ts
@@ -0,0 +1,7 @@
+import Vue, { PluginFunction, VueConstructor } from 'vue';
+
+
+declare const DomleoVue: PluginFunction<any>;
+export default DomleoVue;
+
+export const DomleoVueSample: VueConstructor<Vue>;
